ea3b8b081eb1815671f425181bd8d5c3ede0d1bc
aks2512 <jefferson_akira@hotmail.com>
2021-04-29 21:55:27 -0300

    add descricao de casos de uso e paginas web

---
+++ Web/js/footer.js
@@ -0,0 +1,19 @@
+var content = '<footer style="background-color: #fff;box-shadow: 0px 0px 15px 0px #000000;position: fixed;bottom: 0;width: 100%;" class="d-flex">'
++'<div class="container">'
++'<div class="row justify-content-center align-items-center">'
++'<div class="col-sm-3">'
++'<ul style="list-style: none;margin: 0" class="d-flex justify-content-center align-items-center p-2">'
++'<li><img src="img/facebook.png" width="50px" height="50px" alt=""></li>'
++'<li><img src="img/twitter.png"  width="50px" height="50px" alt=""></li>'
++'<li><img src="img/youtube.png"  width="50px" height="50px" alt=""></li>'
++'</ul>'
++'</div>'
++'<div class="col-sm-3"></div>'
++'<div class="col-sm-6 text-center">@Todos os direitos reservados a JJL</div>'
++'</div>'
++'</div>'
++'</footer>'
++'</footer >'
+
+var footer = document.getElementById("footer")
+footer.innerHTML = content
